eeb93939eedd2edf724be92dae6e6a2f77ae8828
xaves79 <diexave79@gmail.com>
2026-03-24 22:55:21 +0000

    Dockerfile avec richarvey/nginx-php-fpm

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Démarrer PHP-FPM
-php-fpm -D
-
-# Lancer les migrations (optionnel, peut aussi se faire manuellement)
 php artisan migrate --force
-
-# Démarrer Nginx
-nginx -g "daemon off;"
+php-fpm
